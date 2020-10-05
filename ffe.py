cbd2509db51fc562dcbabcf6d84fb0febcf1f436
Felix Paul Werner <felixpaul.werner@hella.com>
2020-10-05 11:20:23 +0200

    Encapsulate each task into function.

--- ffe.py
+++ ffe.py
@@ -5,38 +5,29 @@ URL = ""
 HEADERS = {'content-type': 'application/json'}
 
 def main():
-    # Get all people
-    print("Solution (1):")
-    data = get_request("https://swapi.dev/api/people/", HEADERS)
     # (1) Find Obi and extract name and gender
-    obi = {}
-    for person in data["results"]:
-        if person["name"] == "Obi-Wan Kenobi":
-            print(F" [*] Person found: He is {person['gender']} and is called {person['name']}")
-            # Save his dataset
-            obi = person
+    print("Solution (1):")
+    person = find_person("Obi-Wan Kenobi")
+    print(F" [*] Person found: {person['name']} is {person['gender']}.")
+
     # (2) Find residence on Obi's hometown
     print("Solution (2):")
-    obi_homeworld_endpoint = obi["homeworld"]
-    data = get_request(obi_homeworld_endpoint, HEADERS)
-    for resident in data["residents"]:
-        print(F" [*] {get_name_gender(resident)[0]} is living on Obi's homeworld")
+    residents = find_residents_on_homeworld(person["homeworld"])
+    for resident in residents:
+        print(F" [*] {resident['name']} is living on {person['name']}'s homeworld")
+
     # (3) Find all movies Obi was in
     print("Solution (3):")
-    obi_films = []
-    data = get_request("https://swapi.dev/api/films/", HEADERS)
-    for film in data["results"]:
-        for character in film["characters"]:
-            if character == obi['url']:
-                print(F" [*] Obi was part of: {film['title']}")
-                obi_films.append(film["url"])
-    # (3) Find people that played together with Obi and sort them by occurrence
+    person_films = find_films_of_person(person['name'])
+    for film in person_films:
+        print(F" [*] {person['name']} was part of: {film['title']}")
+
+    # (4) Find people that played together with Obi and sort them by occurrence
     print("Solution (4):")
     obi_friends = {}
-    for film in obi_films:
-        data = get_request(film, HEADERS)
-        for character in data["characters"]:
-            if character == obi['url']:
+    for film in person_films:
+        for character in film["characters"]:
+            if character == person['url']:
                 continue;
             elif character not in obi_friends.keys():
                 obi_friends[character] = 1
@@ -45,11 +36,42 @@ def main():
     for key, value in {k: v for k, v in sorted(obi_friends.items(), key=lambda item: item[1], reverse = True)}.items():
         print(F" [*] {get_name_gender(key)[0]} played {value} times together with Obi")
 
-        
+def find_person(name):
+    data = get_request("https://swapi.dev/api/people/", HEADERS)
+    for person in data["results"]:
+        if person["name"] == name:
+            print(F"Found {person['name']}.")
+            return person
+    print(F"{name} not found.")
+    return None
+
+def find_residents_on_homeworld(homeworld_url_endpoint):
+    residents = []
+    data = get_request(homeworld_url_endpoint, HEADERS)
+    for person_url_endpoint in data["residents"]:
+        data = get_request(person_url_endpoint, HEADERS)
+        residents.append(data)
+    return residents
+
+def find_films_of_person(person):
+    person = find_person(person)
+    films = []
+    data = get_request("https://swapi.dev/api/films/", HEADERS)
+    for film in data["results"]:
+        for worker_url_endpoint in film["characters"]:
+            if worker_url_endpoint == person['url']:
+                films.append(film)
+    return films
+
+
 
 def get_name_gender(url_endpoint):
     data = get_request(url_endpoint, HEADERS)
     return [data["name"], data["gender"]]
+
+
+""" API call helper functions:
+------------------------------"""
 
 def get_request(location, header, parameter = {}):
     request_url = URL + location
